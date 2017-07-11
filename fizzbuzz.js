f6986506cab4fa56f7664c84b5b90d1c47f17d81
Heggy Castaneda <heggyy@gmail.com>
2017-07-11 06:12:19 -0700

    If the number is divisible by 3, print "Fizz".
    If it's not divisible by 3, just print the number.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,11 @@
 // Let's get started by using console.log to print out all of the numbers from 1 and 20.
 
 for(var i = 1; i<=20; i++){
-  console.log(i);
+  if(i%3===0){
+//If the number is divisible by 3, print "Fizz".
+    console.log('Fizz');
+//If it's not divisible by 3, just print the number.
+  } else {
+    console.log(i);
+  }
 }
